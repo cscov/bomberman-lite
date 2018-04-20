fcc618aefbea76fd5149eb750d230bcd28f72463
cscov <carolyn.l.scoville@gmail.com>
2018-04-20 12:04:29 -0700

    Delete computer moves

--- src/computer.js
+++ src/computer.js
@@ -13,7 +13,7 @@ class Computer {
   drawPlayer() {
     this.ctx.drawImage(this.img, this.position.x,
                        this.position.y, 44, 44);
-    // window.setInterval(this.movePlayer.bind(this), 5000);
+    
   }
 
   movePlayer() {
